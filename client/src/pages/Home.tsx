646bf9e579ea788e7f8c4467e5fc80c31fdec286
ah-ns <htvanlierop@loyola.edu>
2025-10-22 16:50:08 -0400

    Calendar showing on home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,11 @@
+import LeagueSchedule from "../components/LeagueSchedule";
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the Baltimore Basketball League</h1>
       <p>Your home for all things basketball in Baltimore!</p>
+      <LeagueSchedule />
     </div>
   );
 }
