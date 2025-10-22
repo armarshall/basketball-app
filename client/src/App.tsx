fee0d17c10d8cf2bd383bb20cf912e2f3c815e09
armarshall <armarsh1023@gmail.com>
2025-10-22 01:33:27 -0400

    Implement routing and layout for the application, adding pages for Home, Rules, Team, Standings, About, Sign Up, and Log In. Introduce a MenuAppBar component for navigation and apply a custom Material UI theme.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MenuAppBar from "./components/MenuAppBar";
+import Home from "./pages/Home";
+import Rules from "./pages/Rules";
+import Team from "./pages/Team";
+import Standings from "./pages/Standings";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <MenuAppBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rules" element={<Rules />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/standings" element={<Standings />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/LogIn.tsx
@@ -0,0 +1,9 @@
+export default function LogIn() {
+  return (
+    <div>
+      <h1>Log In</h1>
+      <p>Login form will be displayed here.</p>
+    </div>
+  );
+}
+
