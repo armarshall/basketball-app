4c65fb9f7703f810fe51426e1d66e9f9cb780e80
ah-ns <htvanlierop@loyola.edu>
2025-10-27 22:48:12 -0400

    team patch for adding player

--- server/src/services/team_service.test.ts
+++ server/src/services/team_service.test.ts
@@ -1,6 +1,11 @@
 import { describe, expect, test, jest } from "@jest/globals";
 
-import { create_team, get_all_teams, get_team_by_id } from "./team_service";
+import {
+  create_team,
+  get_all_teams,
+  get_team_by_id,
+  update_team_players,
+} from "./team_service";
 
 describe("team controller", () => {
   test("get all teams", async () => {
@@ -51,6 +56,26 @@ describe("team controller", () => {
     } as any;
 
     await create_team(req, res);
+    expect(mock_json).toHaveBeenCalled();
+  });
+
+  test("update team players", async () => {
+    const req = {
+      body: "abc", // player to add
+      params: {
+        id: "68fed5dc02763e52d791296f", // team id
+      },
+    } as any;
+
+    const mock_json = jest.fn();
+    const mock_status = jest.fn();
+
+    const res = {
+      json: mock_json,
+      status: mock_status,
+    } as any;
+
+    await update_team_players(req, res);
     expect(mock_json).toHaveBeenCalled();
   });
 });
