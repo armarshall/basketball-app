4c3c65cb914689ec27d386220d3a2b23de32a62b
ah-ns <htvanlierop@loyola.edu>
2025-11-04 00:35:14 -0500

    server side hash validation

--- server/src/routes/guardian_router.ts
+++ server/src/routes/guardian_router.ts
@@ -3,14 +3,20 @@ import {
   create_guardian,
   get_all_guardians,
   get_guardian_by_id,
+  get_guardian_by_email,
+  check_guardian_hash,
 } from "../controllers/guardian_controller";
 
 const router = express.Router();
 
 router.get("/", get_all_guardians);
 
-router.get("/:id", get_guardian_by_id);
+router.get("/id/:id", get_guardian_by_id);
+
+router.get("/email/:email", get_guardian_by_email);
 
 router.post("/", create_guardian);
+
+router.post("/check-hash", check_guardian_hash);
 
 export default router;
